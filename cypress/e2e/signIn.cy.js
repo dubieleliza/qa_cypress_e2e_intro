6b97f4cd1d129ca04e750dadfeefbfe11871e597
Eliza Dubiel <dubieleliza89@gmail.com>
2025-08-18 09:16:02 +0200

    add solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,23 +1,30 @@
 /// <reference types='cypress' />
 
+const primaryUrl = 'https://react-redux.realworld.io/';
+const backupUrl = 'https://conduit.mate.academy/';
+
 describe('Sign in page', () => {
   it('should provide an ability to log in', () => {
-    cy.visit('https://conduit.mate.academy/');
+    // Spróbuj odwiedzić główny URL
+    cy.request({
+      url: primaryUrl,
+      failOnStatusCode: false
+    }).then((response) => {
+      const visitUrl = response.status === 200 ? primaryUrl : backupUrl;
+      cy.visit(visitUrl);
 
-    cy.contains('Sign in')
-      .click();
+      cy.contains('Sign in').click();
 
-    cy.url().should('include', '/login');
+      cy.url().should('include', '/login');
 
-    cy.get('[placeholder="Email"]')
-      .type('mate@mate.com');
-    cy.get('[placeholder="Password"]')
-      .type('Mate.mate');
+      cy.get('[placeholder="Email"]').type('mate@mate.com');
+      cy.get('[placeholder="Password"]').type('Mate.mate');
 
-    cy.contains('[type="submit"]', 'Sign in')
-      .click();
+      cy.contains('[type="submit"]', 'Sign in').click();
 
-    cy.get('nav').contains('mate')
-      .should('be.visible');
+      cy.get('nav', { timeout: 10000 })
+        .contains('mate')
+        .should('be.visible');
+    });
   });
 });
